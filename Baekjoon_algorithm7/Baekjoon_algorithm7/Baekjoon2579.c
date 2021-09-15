bf7a1c63e1e6994246ccbdb6c89b440e697c84d7
Dong <idh1007@naver.com>
2021-09-15 15:42:07 +0900

    백준2579 답..

--- Baekjoon_algorithm7/Baekjoon_algorithm7/Baekjoon2579.c
+++ Baekjoon_algorithm7/Baekjoon_algorithm7/Baekjoon2579.c
@@ -1,27 +1,39 @@
 #include<stdio.h>
-int recur(int stair[], int num, int i, int count,int sum);
-int max = 0;
 
 int main()
 {
 	int num = 0;
 	int stair[300] = { 0, };
-	int count = 0;
-	int sum=0;
-
+	int DP[300];
+	int max = 0;
 	scanf("%d", &num);
 
 	for (int i = 0; i < num; i++)
 	{
 		scanf("%d", &stair[i]);
 	}
-	recur(stair, num, 0, 0,0);
+	DP[0] = stair[0];
+	DP[1] = stair[0] + stair[1];
+	if (stair[0] + stair[2] > stair[1] + stair[2])
+	{
+		DP[2] = stair[0] + stair[2];
+	}
+	else
+	{
+		DP[2] = stair[1] + stair[2];
+	}
+	for (int i = 3; i < num; i++)
+	{
+		if (stair[i] + DP[i-2] > stair[i]+stair[i-1]+DP[i-3])
+		{
+			DP[i] = stair[i] + DP[i - 2];
+		}
+		else
+		{
+			DP[i] = stair[i] + stair[i - 1] + DP[i - 3];
+		}
+	}
 
-	
-	printf("%d", max);
+	printf("%d", DP[num-1]);
 	return 0;
-}
-int recur(int stair[], int num, int i, int count, int sum)
-{
-	
 }
