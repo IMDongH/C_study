180fc02611f85351ed330bd93133e2526a9ea83f
Dong <idh1007@naver.com>
2021-09-24 00:24:58 +0900

    알고리즘 HW2 주석추가

--- algorithm_class_210916_DP3/algorithm_class_210916_DP3/thecheapestway.c
+++ algorithm_class_210916_DP3/algorithm_class_210916_DP3/thecheapestway.c
@@ -1,16 +1,17 @@
 #include<stdio.h>
 
-int fmin(int a, int b, int c);
+int fmin(int a, int b, int c); // A function to find the minimum value.
 int main()
 {
 	int row = 0, cul = 0;
 	int matrix[10][100] = { 0, };
-	int* dp;
-	int* pdp;
-	int list[10][100];
-	int plist[10][100];
+	int* dp; //The minimum values in the current row.
+	int* pdp; //The minimum values in the previous row.
+	int list[10][100]; // List of minimum values in the current row.
+	int plist[10][100]; // List of minimum values in the previous row.
 	int min = 0;
 	int minN = 0;
+
 	FILE* fp;
 	fp = fopen("test.txt", "r");
 	if (fp == NULL)
@@ -31,16 +32,12 @@ int main()
 					fscanf(fp, "%d ", &matrix[i][j]);
 				}
 			}
-
 			dp = (int*)malloc(sizeof(int) * cul);
 			pdp = (int*)malloc(sizeof(int) * cul);
 
-
-
-
 			for (int i = 0; i < row; i++)
 			{
-				if (i == 0)
+				if (i == 0) //Allocating the values in the first row.
 				{
 					for (int j = 0; j < cul; j++)
 					{
@@ -52,14 +49,14 @@ int main()
 				{
 					for (int j = 0; j < cul; j++)
 					{
-						if (j == 0)
+						if (j == 0) //When the position of the object to be compared is 0,
 						{
-							min = fmin(pdp[cul - 1], pdp[j], pdp[j + 1]);
+							min = fmin(pdp[cul - 1], pdp[j], pdp[j + 1]); // find minimum value
 							if (min == pdp[cul - 1])
 							{
-								dp[j] = pdp[cul - 1] + matrix[j][i];
+								dp[j] = pdp[cul - 1] + matrix[j][i]; //Allocate the minimum value at that location.
 								for (int s = 0; s <= i; s++) {
-									list[j][s] = plist[cul - 1][s];
+									list[j][s] = plist[cul - 1][s];	//Path for the minimum value at that location.
 								}
 								list[j][i] = matrix[j][i];
 
@@ -79,7 +76,7 @@ int main()
 								list[j][i] = matrix[j][i];
 							}
 						}
-						else if (j == cul - 1)
+						else if (j == cul - 1) //When the position of the object to be compared is culumn,
 						{
 							min = fmin(pdp[j - 1], pdp[j], pdp[0]);
 							if (min == pdp[j - 1])
@@ -133,9 +130,9 @@ int main()
 
 					}
 				}
-				for (int j = 0; j < cul; j++)
+				for (int j = 0; j < cul; j++) //For the next row, store the current row values in the previous row.
 				{
-					pdp[j] = dp[j];
+					pdp[j] = dp[j]; 
 					for (int s = 0; s < row; s++) {
 						plist[j][s] = list[j][s];
 					}
@@ -144,7 +141,7 @@ int main()
 			}
 
 			min = dp[0];
-			for (int i = 1; i < cul; i++)
+			for (int i = 1; i < cul; i++) //find the location of the minimum value
 			{
 				if (min > dp[i])
 				{
