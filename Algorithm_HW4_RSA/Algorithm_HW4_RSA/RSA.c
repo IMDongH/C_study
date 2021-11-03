8423da9c066d23b051edf7109576cea7bdeb2527
Dong <idh1007@naver.com>
2021-11-04 02:01:16 +0900

    RSA algorithm

--- Algorithm_HW4_RSA/Algorithm_HW4_RSA/RSA.c
+++ Algorithm_HW4_RSA/Algorithm_HW4_RSA/RSA.c
@@ -1,12 +1,101 @@
 #include<stdio.h>
 int GCD(int num1, int num2);
 int extended_Euclid(int num1, int num2, int gcd);
-
+int is_prime(int);
+int mod_Exp(int, int, int);
 void main()
 {
-	extended_Euclid(17, 17, GCD(17, 17));
-}
+	int p, q;
+	int n,N;
+	int M, C;
 
+	
+	while (1)
+	{
+		printf("Enter first prime number : ");
+		scanf("%d", &p);
+		printf("Enter second prime number : ");
+		scanf("%d", &q);
+
+		if (is_prime(p) == 0 || is_prime(q) == 0)
+		{
+			printf("Enter agin !");
+			continue;
+		}
+		else
+		{
+			break;
+		}
+	}
+	printf("Enter encrypt message : ");
+	scanf("%d", &M);
+
+	n = p * q;
+	N = (p - 1) * (q - 1);
+
+	int e = 5;
+	int d;
+
+	int dd;
+	int i = 1;
+	while (1)
+	{
+		dd = (i * N) + 1;
+		if (dd % e == 0)
+		{
+			d = dd / e;
+			break;
+		}
+
+		i++;
+	}
+	//calculate Ciphertext 
+	C = mod_Exp(M, e, n);
+	int D = mod_Exp(C, d, n);
+
+	//output
+	printf("\n Public Key = (%d , %d)\n Secret key = (%d ,%d)\n Ciphertext = %d\n Decrypt Ciphertext = %d\n", e, n, d, n, C, D);
+
+
+}
+int mod_Exp(int a, int b, int n)
+{
+	int c = 0, d = 1;
+
+
+
+	int bit[100000];
+	int count = 0;
+
+	while (b > 0)	//calculate b's binary number
+	{
+		bit[count++] = b % 2;
+		b = b / 2;
+	}
+
+	
+	for (int i = count - 1; i >= 0; i--)
+	{
+		c = 2 * c;
+		d = (d * d) % n;
+		if (bit[i] == 1)
+		{
+			c = c + 1;
+			d = (d * a) % n;
+		}
+	}
+
+	return d;
+}
+int is_prime(int n)
+{
+	for (int i = 2; i < n; i++)
+	{
+		if (n % i == 0)
+			return 0;
+		else return 1;
+	}
+}
 int GCD(int num1, int num2)
 {
 	if (num2 == 0)
@@ -18,6 +107,8 @@ int GCD(int num1, int num2)
 	}
 
 }
+
+
 int extended_Euclid(int num1, int num2, int gcd)
 {
 	int LCM = 0;
